2ff69f7c2b432f99ed43fcaad4fa41384251fe95
Karl <renkai1991920@163.com>
2018-07-31 15:00:55 +0800

    Project14-QuoraDots

---
+++ Project14-QuoraDots/QuoraDots/QuoraDots/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  QuoraDots
+//
+//  Created by Karl on 2018/7/31.
+//  Copyright © 2018年 whisperkarl.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dotOne: UIImageView!
+    @IBOutlet weak var dotTwo: UIImageView!
+    @IBOutlet weak var dotThree: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        startAnimation()
+    }
+    
+    func startAnimation() {
+        dotOne.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+        dotTwo.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+        dotThree.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+        
+        UIView.animate(withDuration: 0.6, delay: 0.0, options: [.repeat, .autoreverse], animations: {
+            self.dotOne.transform = CGAffineTransform.identity
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.6, delay: 0.2, options: [.repeat, .autoreverse], animations: {
+            self.dotTwo.transform = CGAffineTransform.identity
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.6, delay: 0.4, options: [.repeat, .autoreverse], animations: {
+            self.dotThree.transform = CGAffineTransform.identity
+        }, completion: nil)
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
